c74d1b960fc2ea9afab779462a9b02ed8311d8ae
Muhamad Rafli Shidiq <130623661+MuhamadRafliShidiq@users.noreply.github.com>
2026-05-14 05:28:51 +0700

    Update while-doWhile.js

--- while-doWhile.js
+++ while-doWhile.js
@@ -1,9 +1,21 @@
 // Membuat Perulangan While untuk menghasilkan pattern segitiga
 
-Let str = "";
-Let j = 1;
+let str = "";
+let j = 1;
 while ( j <= 4 ) {
   str += "*";
   console.log(str);
   j++
 };
+
+// Membuat Perulangan Do-While untuk menghasilkan pattern segitiga
+
+let str = "";
+let j = 1;
+do {
+  str += "*";
+  console.log(str);
+  j++;
+} while (j <= 4);
+
+
